6a2c38fb5080f5620cf2d54e0590502fbfc2e147
scientiist <josh@teamhydroxide.xyz>
2017-04-13 21:22:45 -0500

    Edit RGBColor

--- datatypes/RGBColor.lua
+++ datatypes/RGBColor.lua
@@ -1,20 +1,13 @@
 local RGBColor = {}
+RGBColor.__index = RGBColor
 
-function RGBColor:_init()
-	o = o or {}
-	setmetatable(o, self)
-	self.__index = self
-	return o
-end
-
-
-function RGBColor:new(r, g, b, a)
-	local s = RGBColor:_init()
-	s.red = (r or 0)
-	s.green = (g or 0)
-	s.blue = (b or 0)
-	s.alpha = (a or 0)
-	return s
+function RGBColor.new(r, g, b, a)
+	local self = setmetatable({}, RGBColor)
+	self.r = r
+	self.g = g
+	self.b = b
+	self.a = a
+	return self
 end
 
 return RGBColor
